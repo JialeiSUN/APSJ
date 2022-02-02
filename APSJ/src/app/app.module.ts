69a65a3119a44555ddb69c5931c5ada915316b81
PaulGorlicki <paul.gorlicki@gmail.com>
2022-02-02 09:49:12 +0100

    Add components

--- APSJ/src/app/app.module.ts
+++ APSJ/src/app/app.module.ts
@@ -3,13 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
+import { MenuComponent } from './Components/menu/menu.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './Components/header/header.component';
+import { SectionComponent } from './Components/section/section.component';
+import { FooterComponent } from './Components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
+    HeaderComponent,
+    SectionComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
